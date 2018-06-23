fdba54faeb1febf7139fd2ad4a525fcfd2fd7ebc
isaac murillo <isaacmurillovega@gmail.com>
2018-06-22 23:04:29 -0600

    Cambio de Nombre Botones

--- Cahuita/Form1.Designer.vb
+++ Cahuita/Form1.Designer.vb
@@ -22,117 +22,117 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
+        Me.lblTitulo = New System.Windows.Forms.Label()
+        Me.lblUsuario = New System.Windows.Forms.Label()
+        Me.tbUsuario = New System.Windows.Forms.TextBox()
+        Me.lblContraseña = New System.Windows.Forms.Label()
+        Me.tbContraseña = New System.Windows.Forms.TextBox()
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.btnOlvidoContraseña = New System.Windows.Forms.Button()
+        Me.btnCrearCuenta = New System.Windows.Forms.Button()
+        Me.btnDesbloquearCuenta = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblTitulo
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Palatino Linotype", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(248, 40)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(262, 65)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "CAHUITA"
+        Me.lblTitulo.AutoSize = True
+        Me.lblTitulo.Font = New System.Drawing.Font("Palatino Linotype", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitulo.Location = New System.Drawing.Point(248, 40)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(262, 65)
+        Me.lblTitulo.TabIndex = 0
+        Me.lblTitulo.Text = "CAHUITA"
         '
-        'Label2
+        'lblUsuario
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(330, 121)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(86, 22)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Usuario:"
+        Me.lblUsuario.AutoSize = True
+        Me.lblUsuario.Font = New System.Drawing.Font("Arial Rounded MT Bold", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUsuario.Location = New System.Drawing.Point(330, 121)
+        Me.lblUsuario.Name = "lblUsuario"
+        Me.lblUsuario.Size = New System.Drawing.Size(86, 22)
+        Me.lblUsuario.TabIndex = 1
+        Me.lblUsuario.Text = "Usuario:"
         '
-        'TextBox1
+        'tbUsuario
         '
-        Me.TextBox1.Location = New System.Drawing.Point(259, 158)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(234, 20)
-        Me.TextBox1.TabIndex = 2
+        Me.tbUsuario.Location = New System.Drawing.Point(259, 158)
+        Me.tbUsuario.Multiline = True
+        Me.tbUsuario.Name = "tbUsuario"
+        Me.tbUsuario.Size = New System.Drawing.Size(234, 20)
+        Me.tbUsuario.TabIndex = 2
         '
-        'Label3
+        'lblContraseña
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(315, 215)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(121, 22)
-        Me.Label3.TabIndex = 3
-        Me.Label3.Text = "Contraseña:"
+        Me.lblContraseña.AutoSize = True
+        Me.lblContraseña.Font = New System.Drawing.Font("Arial Rounded MT Bold", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblContraseña.Location = New System.Drawing.Point(315, 215)
+        Me.lblContraseña.Name = "lblContraseña"
+        Me.lblContraseña.Size = New System.Drawing.Size(121, 22)
+        Me.lblContraseña.TabIndex = 3
+        Me.lblContraseña.Text = "Contraseña:"
         '
-        'TextBox2
+        'tbContraseña
         '
-        Me.TextBox2.Location = New System.Drawing.Point(259, 255)
-        Me.TextBox2.Multiline = True
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(234, 20)
-        Me.TextBox2.TabIndex = 5
+        Me.tbContraseña.Location = New System.Drawing.Point(259, 255)
+        Me.tbContraseña.Multiline = True
+        Me.tbContraseña.Name = "tbContraseña"
+        Me.tbContraseña.Size = New System.Drawing.Size(234, 20)
+        Me.tbContraseña.TabIndex = 5
         '
-        'Button1
+        'btnIngresar
         '
-        Me.Button1.Font = New System.Drawing.Font("Rockwell", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(306, 322)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(142, 30)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = "Ingresar"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnIngresar.Font = New System.Drawing.Font("Rockwell", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIngresar.Location = New System.Drawing.Point(306, 322)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(142, 30)
+        Me.btnIngresar.TabIndex = 6
+        Me.btnIngresar.Text = "Ingresar"
+        Me.btnIngresar.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnOlvidoContraseña
         '
-        Me.Button2.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(306, 438)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(142, 34)
-        Me.Button2.TabIndex = 7
-        Me.Button2.Text = "Olvido contraseña"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnOlvidoContraseña.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOlvidoContraseña.Location = New System.Drawing.Point(306, 438)
+        Me.btnOlvidoContraseña.Name = "btnOlvidoContraseña"
+        Me.btnOlvidoContraseña.Size = New System.Drawing.Size(142, 34)
+        Me.btnOlvidoContraseña.TabIndex = 7
+        Me.btnOlvidoContraseña.Text = "Olvido contraseña"
+        Me.btnOlvidoContraseña.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnCrearCuenta
         '
-        Me.Button3.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button3.Location = New System.Drawing.Point(306, 404)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(142, 28)
-        Me.Button3.TabIndex = 8
-        Me.Button3.Text = "Crear cuenta"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnCrearCuenta.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCrearCuenta.Location = New System.Drawing.Point(306, 404)
+        Me.btnCrearCuenta.Name = "btnCrearCuenta"
+        Me.btnCrearCuenta.Size = New System.Drawing.Size(142, 28)
+        Me.btnCrearCuenta.TabIndex = 8
+        Me.btnCrearCuenta.Text = "Crear cuenta"
+        Me.btnCrearCuenta.UseVisualStyleBackColor = True
         '
-        'Button4
+        'btnDesbloquearCuenta
         '
-        Me.Button4.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button4.Location = New System.Drawing.Point(306, 478)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(142, 40)
-        Me.Button4.TabIndex = 9
-        Me.Button4.Text = "Desbloquear cuenta"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.btnDesbloquearCuenta.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDesbloquearCuenta.Location = New System.Drawing.Point(306, 478)
+        Me.btnDesbloquearCuenta.Name = "btnDesbloquearCuenta"
+        Me.btnDesbloquearCuenta.Size = New System.Drawing.Size(142, 40)
+        Me.btnDesbloquearCuenta.TabIndex = 9
+        Me.btnDesbloquearCuenta.Text = "Desbloquear cuenta"
+        Me.btnDesbloquearCuenta.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 577)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnDesbloquearCuenta)
+        Me.Controls.Add(Me.btnCrearCuenta)
+        Me.Controls.Add(Me.btnOlvidoContraseña)
+        Me.Controls.Add(Me.btnIngresar)
+        Me.Controls.Add(Me.tbContraseña)
+        Me.Controls.Add(Me.lblContraseña)
+        Me.Controls.Add(Me.tbUsuario)
+        Me.Controls.Add(Me.lblUsuario)
+        Me.Controls.Add(Me.lblTitulo)
         Me.Name = "Form1"
         Me.Text = "Login"
         Me.ResumeLayout(False)
@@ -140,13 +140,13 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
+    Friend WithEvents lblTitulo As Label
+    Friend WithEvents lblUsuario As Label
+    Friend WithEvents tbUsuario As TextBox
+    Friend WithEvents lblContraseña As Label
+    Friend WithEvents tbContraseña As TextBox
+    Friend WithEvents btnIngresar As Button
+    Friend WithEvents btnOlvidoContraseña As Button
+    Friend WithEvents btnCrearCuenta As Button
+    Friend WithEvents btnDesbloquearCuenta As Button
 End Class
--- Cahuita/Form1.vb
+++ Cahuita/Form1.vb
@@ -1,3 +1,37 @@
 ﻿Public Class Form1
+    Private Sub BtnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
 
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblUsuario.Click
+
+    End Sub
+
+    Private Sub lblTitulo_Click(sender As Object, e As EventArgs) Handles lblTitulo.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles tbUsuario.TextChanged
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles lblContraseña.Click
+
+    End Sub
+
+    Private Sub btnDesbloquearCuenta_Click(sender As Object, e As EventArgs) Handles btnDesbloquearCuenta.Click
+
+    End Sub
+
+    Private Sub btnOlvidoContraseña_Click(sender As Object, e As EventArgs) Handles btnOlvidoContraseña.Click
+
+    End Sub
+
+    Private Sub btnCrearCuenta_Click(sender As Object, e As EventArgs) Handles btnCrearCuenta.Click
+
+    End Sub
+
+    Private Sub tbContraseña_TextChanged(sender As Object, e As EventArgs) Handles tbContraseña.TextChanged
+
+    End Sub
 End Class
